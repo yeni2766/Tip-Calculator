61748226588aa260929ea81b9f4944c3afafac7d
yeni2766 <yeni.olafeyide@xandertalent.com>
2023-04-01 15:07:30 +0100

    i have added the javascript logic to calculate the tip however i may have questions or help so i may ask oliver since i had some errors

--- javascript.js
+++ javascript.js
@@ -7,6 +7,33 @@ const tipamount = document.querySelector('#tip-amount-value');
 const totalbill = document.querySelector('#total-bill-value');
 const btnreset = document.querySelector('#reset-btn');
 const btntips = document.querySelectorAll('.tip-btn')
+
+
+//making the event for the reset button
+
+btnreset.addEventListener("click",(e)=>{
+    billInput.value = '0';
+    customTip.value = "";
+    noofPeople.value = '1';
+    totalbill.innerHTML = "$0";
+    tipamount.innerHTML = "$0"; 
+
+
+    btntips.forEach((tipbtn) =>{
+       tipbtn.removeAttribute('id');
+
+    });
+    btntips[2].setAttribute("id", "active");//so here every time it resets it goes back to 15%
+});
+
+
+
+
+
+
+
+
+
 //using forEach i should be able to call a function in the array of buttons i have and within this, i done another foreach loop it can listen to each click and change the colour
 
 btntips.forEach((btn) =>{
@@ -18,7 +45,32 @@ btntips.forEach((btn) =>{
     })
 })
 
-//this is to select every button
+
+//now its time to do the calculate the tip value
+let tipPercentage = 0;
+function calculateTip() {
+    const billamount = parseFloat(billInput.value);
+    const peopleNo = parseFloat(noofPeople.value);
+
+    if (customTip.value) {
+        tipPercentage = parseInt(customTip.value);
+    } else {
+        const activeTipBtn = document.querySelector('.tip-btn');
+        tipPercentage = parseInt(activeTipBtn.dataset.tip.replace(/\D/g, ''));
+    }
+
+    const tipAmount = parseFloat((tipPercentage/100) * billamount / peopleNo);
+    const totalbillAmount = parseFloat(billamount / peopleNo) + tipAmount;
+
+    tipamount.innerHTML = `$${tipAmount.toFixed(2)}`;
+    totalbill.innerHTML = `$${totalbillAmount.toFixed(2)}`;
+}
+
+//here we need event listeners to so always monitor every number to press so it can calculate straight away
+
+billInput.addEventListener('input',(event) =>{
+calculateTip();
+})
 
 
 
@@ -27,14 +79,3 @@ btntips.forEach((btn) =>{
 
 
 
-
-
-//making the event for the reset button
-
-btnreset.addEventListener("click",(e)=>{
-billInput.value = '0';
-customTip.value = "";
-noofPeople.value = '1';
-totalbill.innerHTML = "$0";
-tipamount.innerHTML = "$0"; 
-});
