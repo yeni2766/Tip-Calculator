da569d5c132fc5f7aede8f59cbe3d11a2062e9fa
yeni2766 <yeni.olafeyide@xandertalent.com>
2023-04-01 12:13:41 +0100

    added javascript and now i am selecting all of the elements that i will be using from the DOM

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//selecting all of the elements that will be used
+
+const billInput = document.querySelector('#bill-input');
+const customTip = document.querySelector('#customtip');
+const noofPeople = document.querySelector('#peopleinput');
+const tipamount = document.querySelector('tip-amount-value');
+const totalbill = document.querySelector('total-bill-value');
+const btnreset = document.querySelector('.reset-btn');
+
