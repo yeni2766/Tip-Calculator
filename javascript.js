1bff69d3bff1fe40b7cfc96a726c118f618478da
yeni2766 <yeni.olafeyide@xandertalent.com>
2023-04-01 12:26:13 +0100

    i have know added the reset button which works

--- javascript.js
+++ javascript.js
@@ -2,8 +2,17 @@
 
 const billInput = document.querySelector('#bill-input');
 const customTip = document.querySelector('#customtip');
-const noofPeople = document.querySelector('#peopleinput');
-const tipamount = document.querySelector('tip-amount-value');
-const totalbill = document.querySelector('total-bill-value');
-const btnreset = document.querySelector('.reset-btn');
+const noofPeople = document.querySelector('#people-input');
+const tipamount = document.querySelector('#tip-amount-value');
+const totalbill = document.querySelector('#total-bill-value');
+const btnreset = document.querySelector('#reset-btn');
 
+//making the event for the reset button
+
+btnreset.addEventListener("click",(e)=>{
+billInput.value = '0';
+customTip.value = "";
+noofPeople.value = '1';
+totalbill.innerHTML = "$0";
+tipamount.innerHTML = "$0"; 
+});
