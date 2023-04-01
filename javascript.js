473cc873d4c7e7b17ed79e2cfd15ba5b70c68922
yeni2766 <yeni.olafeyide@xandertalent.com>
2023-04-01 12:53:13 +0100

    the buttons now change colour when clicked

--- javascript.js
+++ javascript.js
@@ -6,6 +6,28 @@ const noofPeople = document.querySelector('#people-input');
 const tipamount = document.querySelector('#tip-amount-value');
 const totalbill = document.querySelector('#total-bill-value');
 const btnreset = document.querySelector('#reset-btn');
+const btntips = document.querySelectorAll('.tip-btn')
+//using forEach i should be able to call a function in the array of buttons i have and within this, i done another foreach loop it can listen to each click and change the colour
+
+btntips.forEach((btn) =>{
+    btn.addEventListener('click',(e)=>{
+        btntips.forEach((tipbtn)=>{
+            tipbtn.removeAttribute('id')
+        });
+        event.target.setAttribute("id", "active");
+    })
+})
+
+//this is to select every button
+
+
+
+
+
+
+
+
+
 
 //making the event for the reset button
 
